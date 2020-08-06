2eac6688512bc47ba1d1074f58f2c3f993212d25
Sinute <sinute.lu@gmail.com>
2020-08-07 00:36:47 +0800

    update

---
+++ leetcode/100.same-tree.go
@@ -0,0 +1,94 @@
+package main
+
+import "fmt"
+
+/**
+ * Definition for a binary tree node.
+ */
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+//100. 相同的树
+func isSameTree(p *TreeNode, q *TreeNode) bool {
+	if p == nil && q == nil {
+		return true
+	} else if p == nil && q != nil {
+		return false
+	} else if p != nil && q == nil {
+		return false
+	}
+	return p.Val == q.Val && isSameTree(p.Left, q.Left) && isSameTree(p.Right, q.Right)
+}
+
+func main() {
+	fmt.Println(isSameTree(&TreeNode{
+		Val: 1,
+		Left: &TreeNode{
+			Val:   2,
+			Left:  nil,
+			Right: nil,
+		},
+		Right: &TreeNode{
+			Val:   3,
+			Left:  nil,
+			Right: nil,
+		},
+	}, &TreeNode{
+		Val: 1,
+		Left: &TreeNode{
+			Val:   2,
+			Left:  nil,
+			Right: nil,
+		},
+		Right: &TreeNode{
+			Val:   3,
+			Left:  nil,
+			Right: nil,
+		},
+	}), true)
+	fmt.Println(isSameTree(&TreeNode{
+		Val: 1,
+		Left: &TreeNode{
+			Val:   2,
+			Left:  nil,
+			Right: nil,
+		},
+		Right: nil,
+	}, &TreeNode{
+		Val:  1,
+		Left: nil,
+		Right: &TreeNode{
+			Val:   2,
+			Left:  nil,
+			Right: nil,
+		},
+	}), false)
+	fmt.Println(isSameTree(&TreeNode{
+		Val: 1,
+		Left: &TreeNode{
+			Val:   2,
+			Left:  nil,
+			Right: nil,
+		},
+		Right: &TreeNode{
+			Val:   1,
+			Left:  nil,
+			Right: nil,
+		},
+	}, &TreeNode{
+		Val: 1,
+		Left: &TreeNode{
+			Val:   1,
+			Left:  nil,
+			Right: nil,
+		},
+		Right: &TreeNode{
+			Val:   2,
+			Left:  nil,
+			Right: nil,
+		},
+	}), false)
+}
