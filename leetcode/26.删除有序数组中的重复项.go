17952e1ba8a8e780d212cfe2b07511d85c1d663a
Sinute <sinute.lu@gmail.com>
2022-05-14 15:08:43 +0800

    leetcode 26

---
+++ leetcode/26.删除有序数组中的重复项.go
@@ -0,0 +1,28 @@
+/*
+ * @lc app=leetcode.cn id=26 lang=golang
+ *
+ * [26] 删除有序数组中的重复项
+ */
+
+// @lc code=start
+func removeDuplicates(nums []int) int {
+	if len(nums) <= 1 {
+		return len(nums)
+	}
+
+	i, j := 0, 1
+
+	for j < len(nums) {
+		if nums[i] == nums[j] {
+		} else {
+			nums[i+1] = nums[j]
+			i++
+		}
+		j++
+	}
+
+	return i + 1
+}
+
+// @lc code=end
+
